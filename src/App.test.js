4ab7fbebb52df32c5e7d3c0b7c9b1c60cfda5af3
danielgallo <daniel.gallo@gmail.com>
2020-09-17 11:36:56 +0100

    CI tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Hello World" message', () => {
   const { getByText } = render(<App />);
   const helloMessage = getByText(/hello world/i);
   expect(helloMessage).toBeInTheDocument();
